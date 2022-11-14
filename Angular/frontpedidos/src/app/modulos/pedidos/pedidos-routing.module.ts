700a8d929c61b7fe61ff10af823ac4187a7bc4d7
uaggit99 <ace77ey@gmail.com>
2022-11-13 19:10:33 -0500

    ceracion der utas

--- Angular/frontpedidos/src/app/modulos/pedidos/pedidos-routing.module.ts
+++ Angular/frontpedidos/src/app/modulos/pedidos/pedidos-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AsignarPedidoComponent } from './asignar-pedido/asignar-pedido.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'asignar-pedidos',
+    component : AsignarPedidoComponent
+
+    
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- Angular/frontpedidos/src/app/modulos/seguridad/seguridad-routing.module.ts
+++ Angular/frontpedidos/src/app/modulos/seguridad/seguridad-routing.module.ts
@@ -1,7 +1,25 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CambioClaveComponent } from './cambio-clave/cambio-clave.component';
+import { IdentificacionComponent } from './identificacion/identificacion.component';
+import { RecuperarClaveComponent } from './recuperar-clave/recuperar-clave.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {
+    path :'identificacion',
+    component : IdentificacionComponent
+   },
+   {
+    path: 'cambio.clave',
+    component : CambioClaveComponent
+
+
+   },
+   {
+   path: 'recuperar-clave',
+   component : RecuperarClaveComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
